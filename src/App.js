69d852371ddd2a69d7c97faddb95da0a47176252
mdanhier <danhier.morgan@gmail.com>
2023-09-27 16:53:10 +0200

    renamed component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import ParentComponent from './components/ParentComponent';
-import Ex1Component from './components/ExoComponent';
+import ExoComponent from './components/ExoComponent';
 
 function App() {
   return (
     <div>
       <h1>Mon application React</h1>
-      <ParentComponent/>
-      <Ex1Component/>
+      <ParentComponent />
+      <ExoComponent />
     </div>
   );
 }
